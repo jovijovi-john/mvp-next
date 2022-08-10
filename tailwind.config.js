d3ee0d4ea94c2fac8078ec8d0652243227664a6e
John Víctor <jovijovi2019@hotmail.com>
2022-08-10 11:52:05 -0300

    layout and config tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+
+module.exports = {
+  mode: "jit",
+  content: [
+    "./src/components/**/*.tsx",
+    "./src/pages/**/*.tsx"
+  ],
+  theme: {
+    extend: {}, 
+  },
+  plugins: [
+    require("@tailwindcss/forms")
+  ],
+}
